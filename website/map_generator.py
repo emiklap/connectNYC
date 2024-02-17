de9aa0bfafebe1afdb5649fdd826beae2aca06e1
Natalie Gallo <nataliegallo04@gmail.com>
2024-02-17 13:39:22 -0500

    added logo and map

--- website/map_generator.py
+++ website/map_generator.py
@@ -6,7 +6,7 @@ tooltip = folium.GeoJsonTooltip(fields=["neighborhood"])
 
 popup="temporary value"
 
-folium.GeoJson("pediacitiesnycneighborhoods.geojson", name="nyc", zoom_on_click=True, popup=popup, tooltip=tooltip, 
+folium.GeoJson("website/pediacitiesnycneighborhoods.geojson", name="nyc", zoom_on_click=True, popup=popup, tooltip=tooltip, 
                 highlight_function=lambda feature: {"fillColor": ("green")}).add_to(mapObj)
 
-mapObj.save(outfile="templates/map.html")
+mapObj.save(outfile="website/templates/map.html")
