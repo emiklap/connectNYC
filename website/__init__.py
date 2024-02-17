c09603e491e624382e009c49b9926926fb9445df
dluzong <daphneluzong@gmail.com>
2024-02-17 15:04:34 -0500

    started routes/views

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "grah grah" # not so secret for now ig
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
