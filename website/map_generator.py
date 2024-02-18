00a6a78ba2933ac822632da0ce6e6df65cc85c20
Natalie Gallo <nataliegallo04@gmail.com>
2024-02-18 00:19:33 -0500

    added popups to map

--- website/map_generator.py
+++ website/map_generator.py
@@ -4,9 +4,13 @@ mapObj = folium.Map(location=[40.73112880602221, -73.89060974121095], tiles="Car
 
 tooltip = folium.GeoJsonTooltip(fields=["neighborhood"])
 
-popup="temporary value"
+html = '''<a href="website/templates/home">Select</a>'''
+
+iframe = folium.IFrame(html, width=100, height=100)
+
+popup = folium.Popup(iframe, max_width=100)
 
 folium.GeoJson("website/pediacitiesnycneighborhoods.geojson", name="nyc", zoom_on_click=True, popup=popup, tooltip=tooltip, 
-                highlight_function=lambda feature: {"fillColor": ("green")}).add_to(mapObj)
+               highlight_function=lambda feature: {"fillColor": ("green")}).add_to(mapObj)
 
-mapObj.save(outfile="website/templates/map.html")
+mapObj.save(outfile="website/templates/out_map.html")
